ffdae6ab360b55cf7dafa6b611e332ef039b0e11
Paul <pcherian10@gmail.com>
2018-04-20 19:49:34 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,3 @@
 def display_rainbow(arr)
-
-  display_rainbow =["R:#{arr[0]},R:#{arr[0]},R:#{arr[0]}, 
-
-
-
+  puts "R:#{arr[0]}, O:#{arr[1]}, Y:#{arr[2]}, G:#{arr[3]}, B#{arr[4]}, I:#{arr[5]}, V:#{arr[6]}" 
 end
